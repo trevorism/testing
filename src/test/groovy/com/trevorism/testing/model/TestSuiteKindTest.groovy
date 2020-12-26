5fd5f936f463659a5525618b6957b91b84f0a559
trevorism <trevorism@gmail.com>
2020-12-26 16:09:27 -0500

    Fix error in unit test

--- src/test/groovy/com/trevorism/testing/model/TestSuiteKindTest.groovy
+++ src/test/groovy/com/trevorism/testing/model/TestSuiteKindTest.groovy
@@ -7,7 +7,7 @@ class TestSuiteKindTest {
     @Test
     void testGetValidKind(){
         assert TestSuiteKind.valueOf("unit".toUpperCase())
-        assert TestSuiteKind.valueOf("karma".toUpperCase())
+        assert TestSuiteKind.valueOf("javascript".toUpperCase())
         assert TestSuiteKind.valueOf("cucumber".toUpperCase())
         assert TestSuiteKind.valueOf("web".toUpperCase())
         assert TestSuiteKind.valueOf("powershell".toUpperCase())
