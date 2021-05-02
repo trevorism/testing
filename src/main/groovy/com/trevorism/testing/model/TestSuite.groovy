3d00331ef9b708e47eb5b2a4d025215fed11ee76
trevorism <trevorism@gmail.com>
2021-05-01 23:19:30 -0400

    Update gradle version; add test suite comments

--- src/main/groovy/com/trevorism/testing/model/TestSuite.groovy
+++ src/main/groovy/com/trevorism/testing/model/TestSuite.groovy
@@ -3,8 +3,11 @@ package com.trevorism.testing.model
 class TestSuite {
 
     String id
+    //Easily identify the test suite
     String name
+    //Type of test suite
     String kind
+    //Where the test suite lives
     String source
 
 }
