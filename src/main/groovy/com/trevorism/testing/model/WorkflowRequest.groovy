eea38c337622d41f90be3a720e669660d025d03f
trevorism <trevorism@gmail.com>
2023-05-01 21:56:42 -0400

    Cucumber run in test.yml

--- src/main/groovy/com/trevorism/testing/model/WorkflowRequest.groovy
+++ src/main/groovy/com/trevorism/testing/model/WorkflowRequest.groovy
@@ -3,5 +3,5 @@ package com.trevorism.testing.model
 class WorkflowRequest {
     String branchName = "master"
     String yamlName = "test.yml"
-    String testType = "acceptance"
+    String testType = "cucumber"
 }
